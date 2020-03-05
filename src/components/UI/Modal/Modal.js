9dee72a16ded29b43b967730f59dacb065595914
vika <vika@inflection.com>
2020-03-05 15:55:35 +0200

    Corrected modal markup

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,26 +1,22 @@
-import React, { memo } from 'react';
-
+import React, {memo} from 'react';
 import classes from './Modal.module.css';
 
 
 const Modal = (props) => {
 
-    const ModalContent = (
-        <div>
-            <div className="box-content">
-                <div className="box-wrap">
-                    <div className="row row-gutter-s">
-                        <div className={classes.Modal}>{props.children}</div>
-                    </div>
-                </div>
-            </div>
-            <div className={classes.Overlay}></div>
-        </div>
-    );
+	const ModalContent = (
+		<div>
+			<div className="box-content">
+				<div className="box-wrap">
+					<div className={classes.Modal}>{props.children}</div>
+				</div>
+			</div>
+			<div className={classes.Overlay}></div>
+		</div>
+	);
 
 
-
-    return props.visability ? ModalContent : null;
+	return props.visability ? ModalContent : null;
 };
 
 export default memo(Modal);

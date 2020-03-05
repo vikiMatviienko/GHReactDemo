74eb06cb43c239ce2ed76657e9af0acb97eed87d
vika <vika@inflection.com>
2020-03-05 15:42:09 +0200

    Updates

---
+++ precache-manifest.2eef9e7ffc69c69c46f247f89d7c5090.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54ffce612389504ad7a2605170a84df9",
+    "url": "/GHReactDemo/index.html"
+  },
+  {
+    "revision": "aab9841adf9b56c14e12",
+    "url": "/GHReactDemo/static/css/main.aebab0fb.chunk.css"
+  },
+  {
+    "revision": "77ffc252866e7e1a711e",
+    "url": "/GHReactDemo/static/js/2.0dfc40ad.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/GHReactDemo/static/js/2.0dfc40ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aab9841adf9b56c14e12",
+    "url": "/GHReactDemo/static/js/main.37c81a3d.chunk.js"
+  },
+  {
+    "revision": "10f80ea3959de104c244",
+    "url": "/GHReactDemo/static/js/runtime-main.fcf00caa.js"
+  },
+  {
+    "revision": "0f3c6a2679b121307eedcbcadf96d16f",
+    "url": "/GHReactDemo/static/media/i.0f3c6a26.woff"
+  },
+  {
+    "revision": "7f9fd1958cb16a30be6836cf551ccdc6",
+    "url": "/GHReactDemo/static/media/i.7f9fd195.ttf"
+  },
+  {
+    "revision": "5d4d38087ee40a4b8ccfd4681bab5547",
+    "url": "/GHReactDemo/static/media/sourcesanspro-light.5d4d3808.ttf"
+  },
+  {
+    "revision": "c3c29487ba763066da81c9ddfd7c6ff8",
+    "url": "/GHReactDemo/static/media/sourcesanspro-light.c3c29487.woff"
+  },
+  {
+    "revision": "1cc280d5d86c68ed1a91e80abe816555",
+    "url": "/GHReactDemo/static/media/sourcesanspro-regular.1cc280d5.ttf"
+  },
+  {
+    "revision": "e546c19d00f85f51bf7e7418be887327",
+    "url": "/GHReactDemo/static/media/sourcesanspro-regular.e546c19d.woff"
+  },
+  {
+    "revision": "46393570054c3dcaf02ba6cede6db063",
+    "url": "/GHReactDemo/static/media/sourcesanspro-semibold.46393570.woff"
+  },
+  {
+    "revision": "f1a10b1c9d7584eb30d438ac58793323",
+    "url": "/GHReactDemo/static/media/sourcesanspro-semibold.f1a10b1c.ttf"
+  }
+]);
